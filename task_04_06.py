03626e2b4f8a7e517915ac7a0ca4fb9acf516826
rsv <ena@enagent.ru>
2020-07-12 16:42:42 +0300

    Добавлена строка подтвреждающая, что программа завершена и бесконечных циклов нет.

--- task_04_06.py
+++ task_04_06.py
@@ -3,6 +3,7 @@ from itertools import cycle
 
 digit = int(input('Введите целое число: '))
 str_line = input('Введите строку: ')
+
 # ------------------- Подзадача А ----------------------------
 
 iteratorA = (i for i in count(digit))
@@ -13,21 +14,20 @@ iteratorB = (i for i in cycle(str_line))
 
 # ------------------- Подзадача * ----------------------------
 
-iteratorAB = (el1*el2 for el1 in count(digit) for el2 in cycle(str_line))
+iteratorAB = (el1 * el2 for el1 in count(abs(digit)) for el2 in cycle(str_line))
 
 # Вывод результатов
 
 print(f'{"-" * 20} Результат работы ИТЕРАТОРА А {"-" * 20}')
-for el in range(10):
+for el in range(abs(digit)):
     print(next(iteratorA))
 
 print(f'{"-" * 20} Результат работы ИТЕРАТОРА B {"-" * 20}')
-for el in range(10):
+for el in range(abs(digit)):
     print(next(iteratorB))
 
 print(f'{"-" * 20} Результат работы ИТЕРАТОРА АB {"-" * 20}')
-for el in range(5):
+for el in range(abs(digit)):
     print(next(iteratorAB))
 
-
-
+print(f'{"-" * 10} Работа скрипта завершена. Бесконечных циклов нет.{"-" * 10}')
